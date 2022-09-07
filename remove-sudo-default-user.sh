174c5fd0df6e854cfd6ba6510254ee6ed36eef3a
Atomjoy <atomjoy.official@gmail.com>
2022-09-07 14:45:49 +0200

    Vps

--- remove-sudo-default-user.sh
+++ remove-sudo-default-user.sh
@@ -3,13 +3,13 @@
 # Variables
 VPSUSER=debian
 
-# Remove user from sudo
+# Remove vps default user from sudo
 sudo gpasswd -d $VPSUSER sudo
 
 # Remove sudo users
 echo "" > /etc/sudoers.d/90-cloud-init-users
-echo "# User rules for debian" >> /etc/sudoers.d/90-cloud-init-users
-echo "# debian ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
+echo "# User rules for ${VPSUSER}" >> /etc/sudoers.d/90-cloud-init-users
+echo "# ${VPSUSER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
 
 # Show
 sudo cat /etc/sudoers.d/90-cloud-init-users
